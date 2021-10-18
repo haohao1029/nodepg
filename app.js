2fe6b3d61d971c53e5f07e40d3e3fba85e84c4dc
haohao1029 <ganjinghao2000@gmail.com>
2021-10-18 17:19:42 +0800

    prod

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+const cors = require('cors')
+
 dotenv.config();
 
 // Set up the express app
@@ -9,11 +11,12 @@ const app = express();
 
 // Log requests to the console.
 app.use(logger('dev'));
+app.use(cors());
 
 // Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.options('*', cors())
 // Setup a default catch-all route that sends back a welcome message in JSON format.
 require('./src/routes')(app);
 app.get('*', (req, res) => res.status(200).send({
